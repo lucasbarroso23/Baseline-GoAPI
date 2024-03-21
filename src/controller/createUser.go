4d48c054d947f9233a2b6fd6ad1344322500d8be
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-03-21 11:08:39 +0000

    setup user response and request models

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,25 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lucasbarroso23/baseline-GoAPI/src/configuration/rest_err"
+	"github.com/lucasbarroso23/baseline-GoAPI/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		rest_err := rest_err.NewBadRequestError(
+			fmt.Sprintf("there are some incorrect fields, error=%s\n", err.Error()),
+		)
+
+		c.JSON(rest_err.Code, rest_err)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
