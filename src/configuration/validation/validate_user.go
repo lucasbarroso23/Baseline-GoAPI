930bf5f41f0e5c9b46aa45554ff6fc20848aa329
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-03-22 23:53:40 +0000

    setup validation pkg

---
+++ src/configuration/validation/validate_user.go
@@ -0,0 +1,23 @@
+package validation
+
+import (
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+)
+
+var (
+	Validate = validator.New()
+	transl   ut.Translator
+)
+
+func Init() {
+	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		en := en.New()
+		un := ut.New(en, en)
+		transl, _ = un.GetTranslator("en")
+		en_translations.RegisterDefaultTranslations(val, transl)
+	}
+}
